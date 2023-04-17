26e827cdd11a869f5999589efeaa508d2a01e23c
gillohsylvia <abdallahfatima363@gmail.com>
2023-04-17 13:24:25 -0700

    task-0 revised

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,5 +1,8 @@
 import $ from 'jquery';
 
-$(document).ready(function() {
-	console.log('Hello World!');
-});
+const paragraph1 = $('<p>').text('Holberton Dashboard');
+const paragraph2 = $('<p>').text('Dashboard data for the students');
+const paragraph3 = $('<p>').text('Copyright - Holberton School');
+
+$('body').append(paragraph1, paragraph2, paragraph3);
+
