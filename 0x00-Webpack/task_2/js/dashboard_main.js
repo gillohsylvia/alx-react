0fef66cbc43269a8df3232b5daec2e521ccdbc7b
gillohsylvia <abdallahfatima363@gmail.com>
2023-04-19 08:15:22 -0700

    [Feat: npx build]

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,19 +1,19 @@
 import '../css/main.css';
-import $ from 'jquery';
-import _ from 'lodash';
-
-$("<div id='logo'> </div>").appendTo('body');
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
-$('body').append('<p>Copyright - Holberton School</p>');
+const $ = require('jquery');
+const _ = require('lodash');
 
 let counter = 0;
 
-function updateCounter () {
-  counter++;
-  $("#count").html(`${counter} clicks on the button`);
-};
+$("<div id='logo'> </div>").appendTo('body');
+$("<p>Holberton Dashboard</p>").appendTo('body');
+$("<p>Dashboard data for the students</p>").appendTo('body');
+$("<button>Click here to get started</button>").appendTo('body');
+$("<p id='count'></p>").appendTo('body');
+$("<p>Copyright - Holberton School</p>").appendTo('body');
 
-$('button').on('click', _.debounce(updateCounter, 500));
+function updateCounter() {
+  counter++;
+  $('#count').html(`${counter} clicks on the button`);
+}
+
+$("button").on("click", _.debounce(updateCounter, 500));
